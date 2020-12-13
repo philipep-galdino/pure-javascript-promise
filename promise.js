2e1f68d9a6346930a57ef31139e2b83a47917383
philipep-galdino <philipep.galdino@gmail.com>
2020-12-13 01:13:23 -0300

    created a promise in pure JS

---
+++ promise.js
@@ -0,0 +1,29 @@
+/*
+/   A simple promise example created purely with
+/   Javascript basic functions. 
+/
+/   It will always return a random dog image.
+*/
+
+const url = 'https://dog.ceo/api/breeds/image/random'
+const dogImg = document.querySelector('[data-js="dog-img"]')
+
+const validateHTTPStatus = dogData => {
+    if (!dogData.ok) {
+        throw new Error(`HTTP error, status ${dogData.status}`)
+    }
+    return dogData.json()
+}
+
+const setDogImg = ({ message: url }) => {
+    dogImg.setAttribute('src', url)
+}
+
+const handleRequestError = error => {
+    console.log(error.message)
+}
+
+fetch(url)
+    .then(validateHTTPStatus)
+    .then(setDogImg)
+    .catch(handleRequestError)
